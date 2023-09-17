949a0c3a10e5bebc665938ae234546bfb9966373
tawfik boujeh <tboujeh@gmail.com>
2023-09-17 12:13:37 +0200

    document  get-audio-decoders.sh

--- utils/get-audio-decoders.sh
+++ utils/get-audio-decoders.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+# run this script locally to grep all of the available decoders on your current global ffmpeg version
 ffmpeg -decoders | grep "^ A" | awk -F ' ' '{print $2}' | awk 'NR > 1' | tee audio-decoders
